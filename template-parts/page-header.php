3929167d733986726673e1e96dc39a63b50cad2f
Kevin Bickel <kevin.bickel@colostate.edu>
2023-06-06 14:44:42 -0600

    Page header progress

--- template-parts/page-header.php
+++ template-parts/page-header.php
@@ -1,33 +1,48 @@
 <?php
-$header_image = get_the_post_thumbnail_url( get_the_ID(), 'full' );
-$default_image = get_field('default_image', 'option');
+$featured_image = get_the_post_thumbnail_url( get_the_ID(), 'full' );
+$default_images = get_field('default_images', 'option');
+shuffle($default_images);
+$default_image = $default_images[0];
 $position = get_field('image_position');
-if( has_post_thumbnail() ) { ?>
-	<header class="page-header entry-header custom-header-image" style="background-image:url('<?php echo $header_image; ?>'); background-position: <?php if($position == 'top') {echo 'top';} elseif($position == 'center') {echo 'center';} elseif($position == 'bottom') {echo 'bottom';} ?>">
-<?php } else { ?>
-	<header class="page-header entry-header default-header-image" style="background-image:url('<?php echo $default_image; ?>');">
+$hide_thumbnail = get_field('no_header_image');
+if( has_post_thumbnail() ) {
+	$header_image = $featured_image;
+} else {
+	$header_image = $default_image;
+} ?>
+
+<?php if( $hide_thumbnail ) { // Hide the header image and display just the title and subtitle ?>
+
+<header class="page-header entry-header no-header-image">
+<?php $subtitle = get_field('subtitle'); ?>
+	<div class="title-container" data-aos="fade-up" data-aos-duration="1000">
+		<div class="title-inner">
+			<?php if( $subtitle ): ?>
+				<div class="subtitle-inner">
+					<?php echo '<h2 class="small-title">' . $subtitle . '</h2>'; ?>
+				</div> <!-- .subtitle-container -->
+			<?php endif; ?>
+			<?php the_title( '<h1 class="large-title">', '</h1>' ); ?>
+		</div> <!-- .title-only -->
+	</div> <!-- .title-container -->
+
+</header><!-- .entry-header -->
+
+<?php } else { // Display the header image ?>
+
+<header class="page-header entry-header" style="background-image:url('<?php echo $header_image; ?>'); background-position: <?php if($position == 'top') {echo 'top';} elseif($position == 'center') {echo 'center';} elseif($position == 'bottom') {echo 'bottom';} ?>">
+<?php $subtitle = get_field('subtitle'); ?>
+	<div class="title-container" data-aos="fade-up" data-aos-duration="1000">
+		<div class="title-inner">
+			<?php if( $subtitle ): ?>
+				<div class="subtitle-inner">
+					<?php echo '<h2 class="small-title">' . $subtitle . '</h2>'; ?>
+				</div> <!-- .subtitle-container -->
+			<?php endif; ?>
+			<?php the_title( '<h1 class="large-title">', '</h1>' ); ?>
+		</div> <!-- .title-only -->
+	</div> <!-- .title-container -->
+
+</header><!-- .entry-header -->
+
 <?php } ?>
-
-		<div class="entry-header-wrapper" data-aos="fade-up" data-aos-duration="1000">
-			<?php $subtitle = get_field('subtitle');
-			if( $subtitle ) { ?>
-				<div class="title-subtitle">
-					<div class="subtitle-inner">
-						<?php the_title( '<h1 class="small-title">', '</h1>' ); ?>
-					</div> <!-- .subtitle-container -->
-					<div class="title-container">
-						<div class="title-inner">
-							<?php echo '<h2 class="large-title">' . $subtitle . '</h2>'; ?>
-						</div> <!-- .title-inner -->
-					</div> <!-- .title-container -->
-				</div> <!-- .title-subtitle -->
-			<?php } else { ?>
-				<div class="title-container">
-					<div class="title-inner">
-						<?php the_title( '<h1 class="large-title">', '</h1>' ); ?>
-					</div> <!-- .title-only -->
-				</div> <!-- .title-container -->
-			<?php } ?>
-		</div> <!-- .entry-header-wrapper -->
-
-	</header><!-- .entry-header -->
